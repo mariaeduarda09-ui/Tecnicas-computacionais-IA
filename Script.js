e08ddbb6b6fa93d5b8ee10e0449787a2ba3eb163
mariaeduarda09-ui <maria.zattera@escola.pr.gov.br>
2026-09-11 07:55:44 -0300

    Update Script.js

--- Script.js
+++ Script.js
@@ -64,3 +64,16 @@ function mostraResultado() {
     caixaAlternativas.textContent = "";
 
 }
+
+function aleatorio (lista){
+        const posicao = Math.floor(Math.random()* lista.length);
+        return lista[posicao];
+}
+
+function respostaSelecionada(opcaoSelecionada){
+        const afirmacoes = aleatorio(opcaoSelecionada.afirmacao);
+        historiaFinal += afirmacoes + " ";
+        atual++;
+        mostraPergunta();
+}
+
